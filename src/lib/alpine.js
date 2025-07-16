cbf8d742b0b1d08d0749351bf2e1f18229f5b7b0
Zhengyi Shen <zhengyishen1@gmail.com>
2025-07-15 22:49:28 -0700

    update es6 import, remove htmx

---
+++ src/lib/alpine.js
@@ -0,0 +1,29 @@
+import Alpine from 'alpinejs'
+import mask from '@alpinejs/mask'
+import intersect from '@alpinejs/intersect'
+import persist from '@alpinejs/persist'
+import focus from '@alpinejs/focus'
+import collapse from '@alpinejs/collapse'
+import anchor from '@alpinejs/anchor'
+import morph from '@alpinejs/morph'
+import sort from '@alpinejs/sort'
+import resize from '@alpinejs/resize'
+
+// Register all plugins before starting Alpine
+Alpine.plugin(mask)
+Alpine.plugin(intersect)
+Alpine.plugin(persist)
+Alpine.plugin(focus)
+Alpine.plugin(collapse)
+Alpine.plugin(anchor)
+Alpine.plugin(morph)
+Alpine.plugin(sort)
+Alpine.plugin(resize)
+
+// Make Alpine available globally for debugging (optional)
+window.Alpine = Alpine
+
+// Start Alpine
+Alpine.start()
+
+export default Alpine
