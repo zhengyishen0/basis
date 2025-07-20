d9bc3d92189e49080fcf5541c71c38eca44a77a9
Zhengyi Shen <zhengyishen1@gmail.com>
2025-07-19 22:16:19 -0700

    create grid component

--- src/components/ui/core/index.ts
+++ src/components/ui/core/index.ts
@@ -3,6 +3,7 @@ export { default as Avatar } from "./Avatar.astro";
 export { default as Button } from "./Button.astro";
 export { default as Card } from "./Card.astro";
 export { default as Column } from "./Column.astro";
+export { default as Grid } from "./Grid.astro";
 export { default as Row } from "./Row.astro";
 export { default as List } from "./List.astro";
 export { default as ListItem } from "./ListItem.astro";
