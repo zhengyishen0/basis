946b54c380fa162aa88e291aaf5222ba27436adb
Zhengyi Shen <zhengyishen1@gmail.com>
2025-07-21 11:47:17 -0700

    update the badge component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,43 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        border: "hsl(214.3, 31.8%, 91.4%)",
+        input: "hsl(214.3, 31.8%, 91.4%)",
+        ring: "hsl(222.2, 84%, 4.9%)",
+        background: "hsl(0, 0%, 100%)",
+        foreground: "hsl(222.2, 84%, 4.9%)",
+        primary: {
+          DEFAULT: "hsl(222.2, 47.4%, 11.2%)",
+          foreground: "hsl(210, 40%, 98%)",
+        },
+        secondary: {
+          DEFAULT: "hsl(210, 40%, 96%)",
+          foreground: "hsl(222.2, 84%, 4.9%)",
+        },
+        destructive: {
+          DEFAULT: "hsl(0, 84.2%, 60.2%)",
+          foreground: "hsl(210, 40%, 98%)",
+        },
+        muted: {
+          DEFAULT: "hsl(210, 40%, 96%)",
+          foreground: "hsl(215.4, 16.3%, 46.9%)",
+        },
+        accent: {
+          DEFAULT: "hsl(210, 40%, 96%)",
+          foreground: "hsl(222.2, 84%, 4.9%)",
+        },
+        popover: {
+          DEFAULT: "hsl(0, 0%, 100%)",
+          foreground: "hsl(222.2, 84%, 4.9%)",
+        },
+        card: {
+          DEFAULT: "hsl(0, 0%, 100%)",
+          foreground: "hsl(222.2, 84%, 4.9%)",
+        },
+      },
+    },
   },
   plugins: [],
 }
