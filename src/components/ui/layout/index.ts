d154df9e3e5bfddfc5f7bd5b67d1e451ff7ddb20
Zhengyi Shen <zhengyishen1@gmail.com>
2025-07-21 21:43:06 -0700

    update accordion and list component

--- src/components/ui/layout/index.ts
+++ src/components/ui/layout/index.ts
@@ -3,6 +3,7 @@ export { default as Grid } from './Grid.astro';
 export { default as Row } from './Row.astro';
 export { default as Column } from './Column.astro';
 export { default as List } from './List.astro';
+export { default as ListItem } from './ListItem.astro';
 export { default as Page } from './Page.astro';
 export { default as Section } from './Section.astro';
 export { default as Divider } from './Divider.astro';
