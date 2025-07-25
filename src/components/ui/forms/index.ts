53496e69180946880ea5e2f7784243a214718aa9
Zhengyi Shen <zhengyishen1@gmail.com>
2025-07-25 16:18:01 -0700

    update the radio group component

--- src/components/ui/forms/index.ts
+++ src/components/ui/forms/index.ts
@@ -2,7 +2,12 @@
 export { default as Button } from './Button.astro';
 export { default as Checkbox } from './Checkbox.astro';
 export { default as DatePicker } from './DatePicker.astro';
-export { default as RadioGroup } from './RadioGroup.astro';
+export { default as RadioIndicator } from './RadioIndicator.astro';
+export { default as CheckboxIndicator } from './CheckboxIndicator.astro';
+export { default as RadioGroup } from './radiogroup/RadioGroup.astro';
+export { default as RadioGroupItem } from './radiogroup/RadioGroupItem.astro';
+export { default as RadioGroupLabel } from './radiogroup/RadioGroupLabel.astro';
+export { default as RadioGroupDescription } from './radiogroup/RadioGroupDescription.astro';
 export { default as Slider } from './Slider.astro';
 export { default as Rating } from './Rating.astro';
 export { default as Select } from './Select.astro';
