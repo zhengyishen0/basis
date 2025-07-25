c4b4a733ca60a4127a41836bd37589de64db9b28
Zhengyi Shen <zhengyishen1@gmail.com>
2025-07-25 16:32:55 -0700

    update checkbox indicator

--- src/components/ui/forms/index.ts
+++ src/components/ui/forms/index.ts
@@ -2,8 +2,6 @@
 export { default as Button } from './Button.astro';
 export { default as Checkbox } from './Checkbox.astro';
 export { default as DatePicker } from './DatePicker.astro';
-export { default as RadioIndicator } from './RadioIndicator.astro';
-export { default as CheckboxIndicator } from './CheckboxIndicator.astro';
 export { default as RadioGroup } from './radiogroup/RadioGroup.astro';
 export { default as RadioGroupItem } from './radiogroup/RadioGroupItem.astro';
 export { default as RadioGroupLabel } from './radiogroup/RadioGroupLabel.astro';
@@ -13,3 +11,6 @@ export { default as Rating } from './Rating.astro';
 export { default as Select } from './Select.astro';
 export { default as TextInput } from './TextInput.astro';
 export { default as Textarea } from './Textarea.astro';
+
+// Form Indicators
+export * from '../indicators';
---
+++ src/components/ui/indicators/index.ts
@@ -0,0 +1,3 @@
+// Form Input Indicators
+export { default as RadioIndicator } from './RadioIndicator.astro';
+export { default as CheckboxIndicator } from './CheckboxIndicator.astro';
