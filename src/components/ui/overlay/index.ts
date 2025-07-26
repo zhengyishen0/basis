03aaf9a823edc408ffa3ef5aaafa4190a6ee0d9d
Zhengyi Shen <zhengyishen1@gmail.com>
2025-07-26 11:12:47 -0700

    update right click and clean up the overlay folder

--- src/components/ui/overlay/index.ts
+++ src/components/ui/overlay/index.ts
@@ -2,11 +2,7 @@
 export { default as Command } from './Command.astro';
 export { default as ContextMenu } from './ContextMenu.astro';
 export { default as Dropdown } from './Dropdown.astro';
-export { default as FullScreenModal } from './FullScreenModal.astro';
 export { default as HoverCard } from './HoverCard.astro';
-export { default as Modal } from './Modal.astro';
-export { default as Popover } from './Popover.astro';
-export { default as SlideOver } from './SlideOver.astro';
 
 // Composable overlays
 export * from './dialog';
