d2047ce7c9c1526af4441f365747fe54e4545a96
Zhengyi Shen <zhengyishen1@gmail.com>
2025-07-26 19:36:34 -0700

    final context menu

--- src/components/ui/overlay/contextmenu/index.ts
+++ src/components/ui/overlay/contextmenu/index.ts
@@ -3,6 +3,7 @@ export { default as ContextMenuTrigger } from './ContextMenuTrigger.astro';
 export { default as ContextMenuContent } from './ContextMenuContent.astro';
 export { default as ContextMenuItem } from './ContextMenuItem.astro';
 export { default as ContextMenuGroup } from './ContextMenuGroup.astro';
-export { default as ContextMenuSubmenu } from './ContextMenuSubmenu.astro';
+export { default as ContextMenuSubMenu } from './ContextMenuSubMenu.astro';
 export { default as ContextMenuSubTrigger } from './ContextMenuSubTrigger.astro';
 export { default as ContextMenuShortcut } from './ContextMenuShortcut.astro';
+export { default as ContextMenuConditional } from './ContextMenuConditional.astro';
