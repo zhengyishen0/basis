ca419e4580bc60ab12e221a6a7cb677e5331c6f9
Zhengyi Shen <zhengyishen1@gmail.com>
2025-07-28 13:02:29 -0700

    update command component

--- src/components/ui/overlay/command/index.ts
+++ src/components/ui/overlay/command/index.ts
@@ -1,7 +1,8 @@
 export { default as Command } from './Command.astro';
 export { default as CommandInput } from './CommandInput.astro';
-export { default as CommandGroup } from './CommandGroup.astro';
 export { default as CommandItem } from './CommandItem.astro';
-export { default as CommandContent } from './CommandContent.astro';
-export { default as CommandShortcut } from './CommandShortcut.astro';
 export { default as CommandFooter } from './CommandFooter.astro';
+
+// Re-exports from menu components
+export { default as CommandGroup } from '../menu/MenuGroup.astro';
+export { default as CommandShortcut } from '../menu/MenuShortcut.astro';
