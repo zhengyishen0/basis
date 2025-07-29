70dbcfc684df017b530da879139b4789ac25d4e1
Zhengyi Shen <zhengyishen1@gmail.com>
2025-07-29 13:09:00 -0700

    update the breadcrumb component

--- src/components/ui/navigation/index.ts
+++ src/components/ui/navigation/index.ts
@@ -1,5 +1,6 @@
 // Navigation - Navigation and wayfinding
 export { default as Breadcrumbs } from './Breadcrumbs.astro';
+export { Breadcrumb, BreadcrumbItem } from './breadcrumbs';
 export { default as NavigationMenu } from './NavigationMenu.astro';
 export { default as Pagination } from './Pagination.astro';
 export { default as Tabs } from './Tabs.astro';
