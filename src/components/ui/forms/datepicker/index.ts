50cd8cb4a109f4cbd24e32aa3d3e9d7f7da93451
Zhengyi Shen <zhengyishen1@gmail.com>
2025-07-28 23:11:23 -0700

    create the composable date picker with calendar

---
+++ src/components/ui/forms/datepicker/index.ts
@@ -0,0 +1,5 @@
+// Re-exports from base components - using Popup directly (DropdownMenu pattern)
+export { default as DatePicker } from './DatePicker.astro';
+export { default as DatePickerTrigger } from '../../overlay/popup/PopupTrigger.astro';
+export { default as DatePickerContent } from '../../overlay/popup/PopupContent.astro';
+export { default as DatePickerCalendar } from './DatePickerCalendar.astro';
