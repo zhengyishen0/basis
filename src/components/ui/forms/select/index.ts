ff00b0d46e6ca43237d9fa5a8469f3c68f662183
Zhengyi Shen <zhengyishen1@gmail.com>
2025-07-29 13:09:14 -0700

    update the select component

---
+++ src/components/ui/forms/select/index.ts
@@ -0,0 +1,6 @@
+// Re-exports from base components - using Popup directly
+export { default as Select } from './Select.astro';
+export { default as SelectTrigger } from '../../overlay/popup/PopupTrigger.astro';
+export { default as SelectContent } from '../../overlay/popup/PopupContent.astro';
+export { default as SelectItem } from './SelectItem.astro';
+export { default as SelectGroup } from '../../overlay/menu/MenuGroup.astro';
