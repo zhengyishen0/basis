b470b504bead737a54fec1c95bf7ec5568f60abd
Zhengyi Shen <zhengyishen1@gmail.com>
2025-07-29 18:01:07 -0700

    working on the smart bread crumb

--- src/components/ui/navigation/breadcrumbs/index.ts
+++ src/components/ui/navigation/breadcrumbs/index.ts
@@ -1,2 +1,4 @@
 export { default as Breadcrumb } from './Breadcrumb.astro';
 export { default as BreadcrumbItem } from './BreadcrumbItem.astro';
+export { default as BreadcrumbEllipsis } from './BreadcrumbEllipsis.astro';
+export { default as SmartBreadcrumb } from './SmartBreadcrumb.astro';
